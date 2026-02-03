414b626d91fefce51cff8fde620fa9745de3e3fe
Rich Marafioti <richmarafioti@gmail.com>
2026-02-02 20:07:06 -0600

    feat(_components): updates
    -add responsive theme
    -add independent class styling for reuseable component elements
    -load in site specific icon
    -add is active highlighting to nav elements
    
    Resolves: #5

--- src/app/_data/photos.js
+++ src/app/_data/photos.js
@@ -1,3 +1,13 @@
+export const icon = {
+  alt: "website icon and home page button",
+  src_light:
+    "https://res.cloudinary.com/dzpne110u/image/upload/v1770080963/bss_website/branding/bss_rat_black_stack_r8jba4.svg",
+  src_dark:
+    "https://res.cloudinary.com/dzpne110u/image/upload/v1770080963/bss_website/branding/bss_rat_pink_stack_pkzoo8.svg",
+  height: 360,
+  width: 755,
+};
+
 export const heroPhoto = {
   alt: "Picture of a 1980s desktop computer with the words 'pc view' overlayed ontop of the image",
   desktop: {
